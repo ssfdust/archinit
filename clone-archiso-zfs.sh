b1d4a94325e3c0bb2a54012fa871b98b86306ca1
ssfdust <ssfdust@gmail.com>
2023-01-06 17:33:37 +0800

    update

--- clone-archiso-zfs.sh
+++ clone-archiso-zfs.sh
@@ -2,5 +2,4 @@
 
 git clone https://github.91chi.fun/https://github.com/eoli3n/archiso-zfs ~/archiso-zfs
 
-sed -i "s/archzfs.com/zxcvfdsa.com\/archzfs/" ~/archiso-zfs/init
-
+sed -i "s/archzfs.com/zxcvfdsa.com\/archzfs/g" ~/archiso-zfs/init
