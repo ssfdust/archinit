86492c8e465517c0fc28f6aeda075464692e5745
ssfdust <ssfdust@gmail.com>
2023-01-10 12:30:29 +0800

    update

--- clone-archiso-zfs.sh
+++ clone-archiso-zfs.sh
@@ -3,6 +3,6 @@ rm -rf ~/init
 
 curl -o ~/init -L https://github.91chi.fun/https://github.com/eoli3n/archiso-zfs/blob/master/init
 
-sed -i 's/http:\/\/archzfs.com/https:\/\/mirror.biocrafting.net\/archlinux\/archzfs/g' ~/init
+# sed -i 's/http:\/\/archzfs.com/https:\/\/mirror.biocrafting.net\/archlinux\/archzfs/g' ~/init
 # sed -i 's/http:\/\/archzfs.com/https:\/\/mirror.in.themindsmaze.com\/archzfs/g' ~/init
 # sed -i 's/http:\/\/archzfs.com/https:\/\/zxcvfdsa.com\/archzfs/g' ~/init
