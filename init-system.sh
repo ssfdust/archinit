0391705a9f4cd980f5d846ce65a4ebfdb225a07f
ssfdust <ssfdust@gmail.com>
2023-01-10 11:35:42 +0800

    update

--- init-system.sh
+++ init-system.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-locale-gen
-systemctl enable libvirtd clash@nobody zfs.target zfs-import-cache zfs-zed NetworkManager sddm-plymouth syncthing@ssfdust firewalld
-systemctl disable systemd-resolved
-
-plymouth-set-default-theme -R arch-charge-big
-ln -s /usr/share/zoneinfo/Shanghai /etc/localtime
+rm -f /etc/zfs/zpool.cache
 zpool set cachefile=/etc/zfs/zpool.cache zroot
 # Fix the paths to eliminate /mnt
 sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/*
 zgenhostid $(hostid)
+
+locale-gen
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+plymouth-set-default-theme -R arch-charge-big
+
+cp /home/ssfdust/CloudBox/Sync/apps/clash/* /etc/clash/
+chown -R nobody:nobody /etc/clash
+
+systemctl enable zfs.target zfs-import-cache zfs-import.target zfs-zed \
+    NetworkManager sddm-plymouth firewalld libvirtd \
+    syncthing@ssfdust clash@nobody
+systemctl disable systemd-resolved
